681c7c8b6f6e7c79fb384435e5b8ee68ec45e6f1
jlieth <jlieth@users.noreply.github.com>
2019-05-07 14:48:59 +0200

    logging probably shouldn't be configured in a lib

--- legacy_scrobbler/__init__.py
+++ legacy_scrobbler/__init__.py
@@ -1,12 +1,3 @@
-import logging
-
-import coloredlogs
-
 from legacy_scrobbler.listen import Listen  # noqa: F401
 from legacy_scrobbler.version import __version__, __version_info__  # noqa: F401
 from legacy_scrobbler.clients.legacy import LegacyScrobbler  # noqa: F401
-
-
-# set up logging
-logger = logging.getLogger("legacy_scrobbler")
-coloredlogs.install(level="DEBUG", logger=logger)
